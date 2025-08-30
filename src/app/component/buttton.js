04a8c869122779dfc98a26b6e810024a9127a183
kenrichhbrown <kennethedema5@gmail.com>
2025-08-30 20:56:34 +0100

    first commit

---
+++ src/app/component/buttton.js
@@ -0,0 +1,10 @@
+export default function MyButton({up}){
+  return(
+    <div>
+      <button className="p-3 px-5 bg-black text-white text-2xl">
+        {up}
+      </button>
+    
+    </div>
+  )
+}
